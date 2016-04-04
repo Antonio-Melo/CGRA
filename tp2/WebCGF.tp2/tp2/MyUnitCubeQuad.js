504fc16b3746b2c3e391e88f9fcb9095ad9e4af4
António Melo <happyd3nt@gmail.com>
2016-04-04 17:14:45 +0100

    Added files via upload

---
+++ tp2/WebCGF.tp2/tp2/MyUnitCubeQuad.js
@@ -0,0 +1,52 @@
+/**
+ * MyUnitCubeQuad
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+function MyUnitCubeQuad(scene) {
+	CGFobject.call(this,scene);
+
+    this.quad = new MyQuad(this.scene);
+    this.quad.initBuffers();
+};
+
+MyUnitCubeQuad.prototype = Object.create(CGFobject.prototype);
+MyUnitCubeQuad.prototype.constructor=MyUnitCubeQuad;
+
+MyUnitCubeQuad.prototype.display = function (){
+	var deg2rad=Math.PI/180.0;
+	var a_rad=90.0*deg2rad;
+	var b_rad=360.0*deg2rad;
+
+	//this.scene.translate(2,0,0);
+    this.quad.display();
+    this.scene.pushMatrix();
+	
+    this.scene.translate(0,0,-1);
+    this.quad.display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+    this.scene.rotate(a_rad,0,1,0);
+    this.quad.display();
+    this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.rotate(-a_rad,0,1,0);
+    this.quad.display();
+    this.scene.popMatrix();
+    
+
+    this.scene.pushMatrix();
+    this.scene.translate(0,1,0);
+    this.scene.rotate(a_rad,-1,0,0);
+    this.scene.translate(0,0,-1);
+    this.quad.display();
+    this.scene.popMatrix();
+	
+    this.scene.rotate(a_rad,1,0,0);
+    this.quad.display();
+
+    
+
+}
